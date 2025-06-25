08f23e0f24a634336877ba8f2100f4430d02d3e9
luisg <gabrielnino@gmail.com>
2025-06-24 17:45:26 -0700

    Refactor.

--- Services/Interfaces/IJobDetailsCoordinator.cs
+++
@@ -1,9 +0,0 @@
-﻿using Models;
-
-namespace Services.Interfaces
-{
-    public interface IJobDetailsCoordinator
-    {
-        Task<List<JobOfferDetail>> DetailJobsAsync(List<string> job, string searchText);
-    }
-}
